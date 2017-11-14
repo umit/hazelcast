32703a985c37576bb1b0b3d102759b4462f47a41
Ensar Basri Kahveci <ebkahveci@gmail.com>
2017-11-14 16:39:01 +0300

    Move raft config params to RaftConfig

--- hazelcast-raft/src/main/java/com/hazelcast/raft/RaftConfig.java
+++ hazelcast-raft/src/main/java/com/hazelcast/raft/RaftConfig.java
@@ -3,22 +3,101 @@ package com.hazelcast.raft;
 import java.util.Collection;
 import java.util.HashSet;
 
+import static com.hazelcast.util.Preconditions.checkPositive;
+import static com.hazelcast.util.Preconditions.checkTrue;
+
 /**
  * TODO: Javadoc Pending...
  *
  */
 public class RaftConfig {
 
+
+    public static final long DEFAULT_LEADER_ELECTION_TIMEOUT_IN_MILLIS = 2000;
+
+    public static final long DEFAULT_LEADER_HEARTBEAT_PERIOD_IN_MILLIS = 5000;
+
+    public static final int DEFAULT_APPEND_REQUEST_MAX_ENTRY_COUNT = 50;
+
+    public static final int DEFAULT_COMMIT_INDEX_ADVANCE_COUNT_TO_SNAPSHOT = 1000;
+
+    public static final int DEFAULT_UNCOMMITTED_ENTRY_COUNT_TO_REJECT_NEW_APPENDS = 100;
+
+
+
+    private long leaderElectionTimeoutInMillis = DEFAULT_LEADER_ELECTION_TIMEOUT_IN_MILLIS;
+
+    private long leaderHeartbeatPeriodInMillis = DEFAULT_LEADER_HEARTBEAT_PERIOD_IN_MILLIS;
+
+    private int appendRequestMaxEntryCount = DEFAULT_APPEND_REQUEST_MAX_ENTRY_COUNT;
+
+    private int commitIndexAdvanceCountToSnapshot = DEFAULT_COMMIT_INDEX_ADVANCE_COUNT_TO_SNAPSHOT;
+
+    private int uncommittedEntryCountToRejectNewAppends = DEFAULT_UNCOMMITTED_ENTRY_COUNT_TO_REJECT_NEW_APPENDS;
+
     private final Collection<RaftMember> members = new HashSet<RaftMember>();
+
+    public long getLeaderElectionTimeoutInMillis() {
+        return leaderElectionTimeoutInMillis;
+    }
+
+    public RaftConfig setLeaderElectionTimeoutInMillis(long leaderElectionTimeoutInMillis) {
+        checkPositive(leaderElectionTimeoutInMillis, "leader election timeout in millis: "
+                + leaderElectionTimeoutInMillis + " should be positive");
+        this.leaderElectionTimeoutInMillis = leaderElectionTimeoutInMillis;
+        return this;
+    }
+
+    public long getLeaderHeartbeatPeriodInMillis() {
+        return leaderHeartbeatPeriodInMillis;
+    }
+
+    public RaftConfig setLeaderHeartbeatPeriodInMillis(long leaderHeartbeatPeriodInMillis) {
+        checkPositive(leaderHeartbeatPeriodInMillis, "leader heartbeat period in millis: "
+                + leaderHeartbeatPeriodInMillis + " should be positive");
+        this.leaderHeartbeatPeriodInMillis = leaderHeartbeatPeriodInMillis;
+        return this;
+    }
+
+    public int getAppendRequestMaxEntryCount() {
+        return appendRequestMaxEntryCount;
+    }
+
+    public RaftConfig setAppendRequestMaxEntryCount(int appendRequestMaxEntryCount) {
+        checkPositive(appendRequestMaxEntryCount, "append request max entry count: " + appendRequestMaxEntryCount
+                + " should be positive");
+        this.appendRequestMaxEntryCount = appendRequestMaxEntryCount;
+        return this;
+    }
+
+    public int getCommitIndexAdvanceCountToSnapshot() {
+        return commitIndexAdvanceCountToSnapshot;
+    }
+
+    public RaftConfig setCommitIndexAdvanceCountToSnapshot(int commitIndexAdvanceCountToSnapshot) {
+        checkPositive(commitIndexAdvanceCountToSnapshot, "commit index advance count to snapshot: "
+                + commitIndexAdvanceCountToSnapshot + " should be positive");
+        this.commitIndexAdvanceCountToSnapshot = commitIndexAdvanceCountToSnapshot;
+        return this;
+    }
+
+    public int getUncommittedEntryCountToRejectNewAppends() {
+        return uncommittedEntryCountToRejectNewAppends;
+    }
+
+    public RaftConfig setUncommittedEntryCountToRejectNewAppends(int uncommittedEntryCountToRejectNewAppends) {
+        checkPositive(uncommittedEntryCountToRejectNewAppends, "uncommitted entry count to reject new appends: "
+                + uncommittedEntryCountToRejectNewAppends + " should be positive");
+        this.uncommittedEntryCountToRejectNewAppends = uncommittedEntryCountToRejectNewAppends;
+        return this;
+    }
 
     public Collection<RaftMember> getMembers() {
         return members;
     }
 
     public RaftConfig setMembers(Collection<RaftMember> members) {
-        if (members.size() < 2) {
-            throw new IllegalArgumentException("Raft groups must have at least 2 members");
-        }
+        checkTrue(members.size() < 2, "Raft groups must have at least 2 members");
 
         this.members.clear();
         this.members.addAll(members);
