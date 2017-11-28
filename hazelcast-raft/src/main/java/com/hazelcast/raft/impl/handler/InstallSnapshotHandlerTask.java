8e194f1a9c0dccff2c1b941dee02a2482250f84d
Ensar Basri Kahveci <ebkahveci@gmail.com>
2017-11-28 11:20:16 +0300

    Fix snapshotting bug
    * InstallSnapshot RPC should contain leader's current term to do the term checks
    * Fix bug in term check in AppendEntries RPC handler

--- hazelcast-raft/src/main/java/com/hazelcast/raft/impl/handler/InstallSnapshotHandlerTask.java
+++ hazelcast-raft/src/main/java/com/hazelcast/raft/impl/handler/InstallSnapshotHandlerTask.java
@@ -2,6 +2,7 @@ package com.hazelcast.raft.impl.handler;
 
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.raft.impl.RaftNode;
+import com.hazelcast.raft.impl.dto.AppendFailureResponse;
 import com.hazelcast.raft.impl.dto.AppendSuccessResponse;
 import com.hazelcast.raft.impl.dto.InstallSnapshot;
 import com.hazelcast.raft.impl.log.LogEntry;
@@ -38,33 +39,33 @@ public class InstallSnapshotHandlerTask implements StripedRunnable {
         }
 
         LogEntry snapshot = req.snapshot();
+
         // Reply false if term < currentTerm (§5.1)
-        if (snapshot.term() < state.term()) {
+        if (req.term() < state.term()) {
             logger.warning("Stale snapshot: " + req + " received in current term: " + state.term());
+            AppendFailureResponse resp = new AppendFailureResponse(raftNode.getLocalEndpoint(), state.term(), snapshot.index() + 1);
+            raftNode.send(resp, req.leader());
             return;
         }
 
-        // Increase the term if we see a newer one, also transition to follower
-        if (snapshot.term() > state.term()) {
+        // Increase the term if we see a newer one, also transform to follower
+        if (req.term() > state.term()) {
             // If RPC request or response contains term T > currentTerm: set currentTerm = T, convert to follower (§5.1)
-            logger.info("Demoting to FOLLOWER from current term: " + state.term() + " to new term: " + snapshot.term()
+            logger.info("Demoting to FOLLOWER from current term: " + state.term() + " to new term: " + req.term()
                     + " and leader: " + req.leader());
-            state.toFollower(snapshot.term());
-            state.leader(req.leader());
+            state.toFollower(req.term());
+            raftNode.printMemberState();
         }
 
         if (!req.leader().equals(state.leader())) {
             logger.info("Setting leader: " + req.leader());
             state.leader(req.leader());
+            raftNode.printMemberState();
         }
 
-        if (snapshot.index() <= state.commitIndex()) {
-            logger.warning("Ignored stale snapshot: " + snapshot + ". commit index: " + state.commitIndex());
-            return;
+        if (raftNode.installSnapshot(snapshot)) {
+            raftNode.send(new AppendSuccessResponse(raftNode.getLocalEndpoint(), req.term(), snapshot.index()), req.leader());
         }
-
-        raftNode.installSnapshot(snapshot);
-        raftNode.send(new AppendSuccessResponse(raftNode.getLocalEndpoint(), snapshot.term(), snapshot.index()), req.leader());
     }
 
 }
