1211351e0ba757534834e4b68c86d811847a35ca
Mehmet Dogan <mehmet@dogan.io>
2017-12-21 15:44:19 +0300

    Add Raft state javadocs

--- hazelcast-raft/src/main/java/com/hazelcast/raft/impl/state/RaftGroupMembers.java
+++ hazelcast-raft/src/main/java/com/hazelcast/raft/impl/state/RaftGroupMembers.java
@@ -9,8 +9,8 @@ import java.util.Set;
 import static java.util.Collections.unmodifiableSet;
 
 /**
- * TODO: Javadoc Pending...
- *
+ * Immutable container for members of a Raft group with an index identifying
+ * membership change's position in the Raft log.
  */
 public class RaftGroupMembers {
 
@@ -28,26 +28,46 @@ public class RaftGroupMembers {
         this.remoteMembers = unmodifiableSet(remoteMembers);
     }
 
+    /**
+     * Returns the position of the membership change that leads to formation of this group.
+     */
     public int index() {
         return index;
     }
 
+    /**
+     * Return all members in this group.
+     *
+     * @see #remoteMembers()
+     */
     public Collection<RaftEndpoint> members() {
         return members;
     }
 
+    /**
+     * Returns remote members in this group, excluding the local member.
+     */
     public Collection<RaftEndpoint> remoteMembers() {
         return remoteMembers;
     }
 
+    /**
+     * Returns the number of members in this group.
+     */
     public int memberCount() {
         return members.size();
     }
 
+    /**
+     * Returns the majority for this group.
+     */
     public int majority() {
         return members.size() / 2 + 1;
     }
 
+    /**
+     * Returns true if the endpoint is a member of this group, false otherwise.
+     */
     public boolean isKnownEndpoint(RaftEndpoint endpoint) {
         return members.contains(endpoint);
     }
--- hazelcast-raft/src/main/java/com/hazelcast/raft/impl/state/RaftState.java
+++ hazelcast-raft/src/main/java/com/hazelcast/raft/impl/state/RaftState.java
@@ -9,35 +9,89 @@ import com.hazelcast.raft.impl.log.RaftLog;
 import java.util.Collection;
 
 /**
- * TODO: Javadoc Pending...
- *
+ * {@code RaftState} is the mutable state maintained by Raft state machine on every node in the group.
  */
 public class RaftState {
 
+    /**
+     * Endpoint of this node
+     */
     private final RaftEndpoint localEndpoint;
+
+    /**
+     * Group id
+     */
     private final RaftGroupId groupId;
+
+    /**
+     * Latest committed group members
+     */
     private RaftGroupMembers committedGroupMembers;
+
+    /**
+     * Latest applied group members (initially equal to {@link #committedGroupMembers})
+     */
     private RaftGroupMembers lastGroupMembers;
 
+    /**
+     * Role of this node in the group
+     */
     private RaftRole role = RaftRole.FOLLOWER;
+
+    /**
+     * Latest term this node has seen (initialized to 0 on first boot, increases monotonically)
+     */
     private int term;
 
+    /**
+     * Latest known leader endpoint (or null if not known).
+     */
     private volatile RaftEndpoint leader;
 
-    // index of highest committed log entry
+    /**
+     * Index of highest log entry known to be committed (initialized to 0, increases monotonically)
+     */
     private int commitIndex;
 
-    // index of highest log entry that's applied to state
-    // lastApplied <= commitIndex
+    /**
+     * Index of highest log entry applied to state machine (initialized to 0, increases monotonically)
+     * <p>
+     * {@code lastApplied <= commitIndex} condition holds true always.
+     */
     private int lastApplied;
 
+    /**
+     * Endpoint that received vote in {@link #lastVoteTerm} (or null if none)
+     */
     private RaftEndpoint votedFor;
+
+    /**
+     * Term that granted vote for {@link #votedFor}
+     */
     private int lastVoteTerm;
 
+    /**
+     * Raft log entries; each entry contains command for state machine,
+     * and term when entry was received by leader (first index is 1)
+     */
     private RaftLog log = new RaftLog();
 
+    /**
+     * State maintained by the leader, null if this node is not the leader
+     */
     private LeaderState leaderState;
+
+    /**
+     * Candidate state maintained during pre-voting,
+     * becomes null when pre-voting ends by one of {@link #toCandidate()}, {@link #toLeader()} or {@link #toFollower(int)}.
+     */
     private CandidateState preCandidateState;
+
+    /**
+     * Candidate state maintained during leader election,
+     * initialized when this node becomes candidate via {@link #toCandidate()}
+     * and becomes null when voting ends by one of {@link #toLeader()} or {@link #toFollower(int)}.
+     */
     private CandidateState candidateState;
 
     public RaftState(RaftGroupId groupId, RaftEndpoint localEndpoint, Collection<RaftEndpoint> endpoints) {
@@ -58,97 +112,174 @@ public class RaftState {
         return groupId;
     }
 
+    /**
+     * Returns all members in the last applied group members
+     */
     public Collection<RaftEndpoint> members() {
         return lastGroupMembers.members();
     }
 
+    /**
+     * Returns remote members in the last applied group members
+     */
     public Collection<RaftEndpoint> remoteMembers() {
         return lastGroupMembers.remoteMembers();
     }
 
+    /**
+     * Returns number of members in the last applied group members
+     */
     public int memberCount() {
         return lastGroupMembers.memberCount();
     }
 
+    /**
+     * Returns majority number of the last applied group members
+     */
     public int majority() {
         return lastGroupMembers.majority();
     }
 
+    /**
+     * Returns log index of the last applied group members
+     */
     public int membersLogIndex() {
         return lastGroupMembers.index();
     }
 
+    /**
+     * Returns committed group members
+     */
     public RaftGroupMembers committedGroupMembers() {
         return committedGroupMembers;
     }
 
+    /**
+     * Returns the last applied group members
+     */
     public RaftGroupMembers lastGroupMembers() {
         return lastGroupMembers;
     }
 
+    /**
+     * Returns role of this node in the group.
+     */
     public RaftRole role() {
         return role;
     }
 
+    /**
+     * Returns the latest term this node has seen
+     */
     public int term() {
         return term;
     }
 
+    /**
+     * Increment term by 1
+     */
     int incrementTerm() {
         return ++term;
     }
 
+    /**
+     * Returns the known leader
+     */
     public RaftEndpoint leader() {
         return leader;
     }
 
+    /**
+     * Returns the term when this note voted for endpoint {@link #votedFor}
+     * @see #lastVoteTerm
+     */
     public int lastVoteTerm() {
         return lastVoteTerm;
     }
 
+    /**
+     * Returns the endpoint this note voted for
+     * @see #votedFor
+     */
     public RaftEndpoint votedFor() {
         return votedFor;
     }
 
+    /**
+     * Updates the known leader
+     */
     public void leader(RaftEndpoint endpoint) {
         leader = endpoint;
     }
 
+    /**
+     * Returns the index of highest log entry known to be committed
+     * @see #commitIndex
+     */
     public int commitIndex() {
         return commitIndex;
     }
 
+    /**
+     * Updates commit index
+     * @see #commitIndex
+     */
     public void commitIndex(int index) {
         assert index >= commitIndex : "new commit index: " + index + " is smaller than current commit index: " + commitIndex;
         commitIndex = index;
     }
 
+    /**
+     * Returns the index of highest log entry applied to state machine
+     * @see #lastApplied
+     */
     public int lastApplied() {
         return lastApplied;
     }
 
+    /**
+     * Updates the last applied index
+     * @see #lastApplied
+     */
     public void lastApplied(int index) {
         assert index >= lastApplied : "new last applied: " + index + " is smaller than current last applied: " + lastApplied;
         lastApplied = index;
     }
 
+    /**
+     * Returns the Raft log
+     */
     public RaftLog log() {
         return log;
     }
 
+    /**
+     * Returns the leader state
+     */
     public LeaderState leaderState() {
         return leaderState;
     }
 
+    /**
+     * Returns the candidate state
+     */
     public CandidateState candidateState() {
         return candidateState;
     }
 
+    /**
+     * Persist a vote for the endpoint in current term during leader election.
+     */
     public void persistVote(int term, RaftEndpoint endpoint) {
         this.lastVoteTerm = term;
         this.votedFor = endpoint;
     }
 
+    /**
+     * Switches this node to follower role. Clears leader and (pre)candidate states, updates the term.
+     *
+     * @param term current term
+     */
     public void toFollower(int term) {
         role = RaftRole.FOLLOWER;
         leader = null;
@@ -158,6 +289,12 @@ public class RaftState {
         this.term = term;
     }
 
+    /**
+     * Switches this node to candidate role. Clears pre-candidate and leader states.
+     * Initializes candidate state for current majority and grants vote for local endpoint as a candidate.
+     *
+     * @return vote request to sent to other members during leader election
+     */
     public VoteRequest toCandidate() {
         role = RaftRole.CANDIDATE;
         preCandidateState = null;
@@ -169,6 +306,10 @@ public class RaftState {
         return new VoteRequest(localEndpoint, term, log.lastLogOrSnapshotTerm(), log.lastLogOrSnapshotIndex());
     }
 
+    /**
+     * Switches this node to leader role. Sets local endpoint as current leader.
+     * Clears (pre)candidate states. Initializes leader state for current members.
+     */
     public void toLeader() {
         role = RaftRole.LEADER;
         leader(localEndpoint);
@@ -177,19 +318,39 @@ public class RaftState {
         leaderState = new LeaderState(lastGroupMembers.remoteMembers(), log.lastLogOrSnapshotIndex());
     }
 
+    /**
+     * Returns true if the endpoint is a member of the last applied group, false otherwise.
+     */
     public boolean isKnownEndpoint(RaftEndpoint endpoint) {
         return lastGroupMembers.isKnownEndpoint(endpoint);
     }
 
+    /**
+     * Initializes pre-candidate state for pre-voting and grant a vote for local endpoint
+     */
     public void initPreCandidateState() {
         preCandidateState = new CandidateState(majority());
         preCandidateState.grantVote(localEndpoint);
     }
 
+    /**
+     * Returns pre-candidate state
+     */
     public CandidateState preCandidateState() {
         return preCandidateState;
     }
 
+    /**
+     * Initializes the last applied group members with the endpoints and logIndex.
+     * This method expects there's no uncommitted membership changes, committed members are the same as
+     * the last applied members.
+     *
+     * Leader state is updated for the members which don't exist in committed members and committed members
+     * those don't exist in latest applied members are removed.
+     *
+     * @param logIndex log index of membership change
+     * @param endpoints latest applied endpoints
+     */
     public void updateGroupMembers(int logIndex, Collection<RaftEndpoint> endpoints) {
         assert committedGroupMembers == lastGroupMembers
                 : "Cannot update group members to: " + endpoints + " at log index: " + logIndex + " because last group members: "
@@ -217,6 +378,10 @@ public class RaftState {
         }
     }
 
+    /**
+     * Marks the last applied group members as committed. At this point {@link #committedGroupMembers}
+     * and {@link #lastGroupMembers} are the same.
+     */
     public void commitGroupMembers() {
         assert committedGroupMembers != lastGroupMembers
                 : "Cannot commit last group members: " + lastGroupMembers + " because it is same with committed group members";
@@ -224,6 +389,10 @@ public class RaftState {
         committedGroupMembers = lastGroupMembers;
     }
 
+    /**
+     * Resets {@link #lastGroupMembers} back to {@link #committedGroupMembers}. Essentially this means,
+     * applied but uncommitted membership changes are reverted.
+     */
     public void resetGroupMembers() {
         assert this.committedGroupMembers != this.lastGroupMembers;
 
@@ -231,6 +400,10 @@ public class RaftState {
         // there is no leader state to clean up
     }
 
+    /**
+     * Restores group members from the snapshot. Both {@link #committedGroupMembers}
+     * and {@link #lastGroupMembers} are overwritten and they become the same.
+     */
     public void restoreGroupMembers(int logIndex, Collection<RaftEndpoint> endpoints) {
         assert committedGroupMembers == lastGroupMembers
                 : "Cannot restore group members to: " + endpoints + " at log index: " + logIndex + " because last group members: "
