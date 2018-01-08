bb34e76c2035d291759fb95acea24545b615a607
Mehmet Dogan <mehmet@dogan.io>
2018-01-08 15:02:44 +0300

    Fix Raft group partitionId calculation
    - Added a few javadocs

--- hazelcast/src/main/java/com/hazelcast/spi/GracefulShutdownAwareService.java
+++ hazelcast/src/main/java/com/hazelcast/spi/GracefulShutdownAwareService.java
@@ -3,11 +3,20 @@ package com.hazelcast.spi;
 import java.util.concurrent.TimeUnit;
 
 /**
- * TODO: Javadoc Pending...
- *
+ * An interface that can be implemented by SPI services to participate in graceful shutdown process, such as moving
+ * their internal data to another node or releasing their allocated resources gracefully.
  */
 public interface GracefulShutdownAwareService {
 
+    /**
+     * A hook method that's called during graceful shutdown to provide safety for data managed by this service.
+     * Shutdown process is blocked until this method returns or shutdown timeouts. If this method does not
+     * return in time, then it's considered as failed to gracefully shutdown.
+     *
+     * @param timeout timeout for graceful shutdown
+     * @param unit time unit
+     * @return true if graceful shutdown is successful, false otherwise
+     */
     boolean onShutdown(long timeout, TimeUnit unit);
 
 }
